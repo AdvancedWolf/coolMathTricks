56e333c4db7e51a71f8d098f7b4c4adde34c695d
AdvancedWolf <wolf36842@gmail.com>
2026-09-08 12:15:47 +0300

    generalized the shoelace formula

--- coolMathTricks/coolMathTricks/main.cpp
+++ coolMathTricks/coolMathTricks/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <utility>
+#include <cmath>
 
 double getGaussArea();
 bool includeRoot(const std::vector<std::pair<double, double>>& points);
@@ -16,21 +17,23 @@ double getGaussArea()
 {
 	std::vector<std::pair<double, double>> points;
 	int count = 0;
+	std::cout << "Enter the number of points:" << std::endl;
 	std::cin >> count;
 	int x = 0, y = 0;
+	std::cout << "Enter the coordinates counter-clockwise:" << std::endl;
 	for (size_t i = 0; i < count; ++i)
 	{
-		std::cout << "Enter the coordinates counter-clockwise:" << std::endl;
 		std::cin >> x >> y;
 		points.push_back({x, y});
 	}
 	points.push_back(points.front());
-	double res = 0;
+	double S1 = 0, S2 = 0;
 	for (size_t i = 0; i < points.size() - 1; ++i)
 	{
-		res += points[i].first * points[i+1].second - points[i].second * points[i + 1].first;
+		S1 += points[i].first * points[i + 1].second;
+		S2 += points[i].second * points[i + 1].first;
 	}
-	return 0.5 * res;
+	return 0.5 * std::abs(S1 - S2);
 }
 
 bool includeRoot(const std::vector<std::pair<double, double>>& points)
